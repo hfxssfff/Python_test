176c73f9cf149de86c1f291d8790a812a2ab5cbe
hfxssfff <ylxssnoah@163.com>
2017-07-03 14:03:26 +0800

    modified:sniff_count

--- Sniff.py
+++ Sniff.py
@@ -1,6 +1,6 @@
 #安装依赖包 pip install scapy-python3
 from scapy.all import *
-pcap = sniff(iface=conf.iface,count=4)
+pcap = sniff(iface=conf.iface,count=50)
 print(pcap)
 print(conf.iface)
 wrpcap('demo.pcap', pcap)
